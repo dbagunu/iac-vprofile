56d730ccba6ca53128a9a07ccaa2807d2237c8a1
dbagunu <dcmbagunu@tutanota.com>
2024-02-17 19:05:39 +0800

    Enabled tfstate upload to S3

--- refactored/global/tfstate_backend/main.tf
+++ refactored/global/tfstate_backend/main.tf
@@ -1,15 +1,15 @@
-/* terraform {
+terraform {
   # Backend where the tfstate file will be saved
 
   backend "s3" {
     bucket = "terraform-aws-vprofile"
-    key = "tfstate-backend/terraform.tfstate"
+    key    = "tfstate-backend/terraform.tfstate"
     region = "ap-southeast-1"
 
     dynamodb_table = "terraform-state-locks2"
-    encrypt = true
+    encrypt        = true
   }
-} */
+}
 
 
 
