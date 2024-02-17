01cc20f8e1666c74d9288a74a67f135187316b61
dbagunu <dcmbagunu@tutanota.com>
2024-02-17 17:37:04 +0800

    Refactored code from terraform folder for easy reading

---
+++ refactored/live/main.tf
@@ -0,0 +1,79 @@
+terraform {
+  # Backend where the tfstate file will be saved
+
+  backend "s3" {
+    bucket = "terraform-aws-vprofile"
+    key = "staging/terraform.tfstate"
+    region = "ap-southeast-1"
+
+    dynamodb_table = "terraform-state-locks2"
+    encrypt = true
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  name = "vprofile-eks"
+
+  cidr = "172.20.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
+  public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+}
+
+module "eks" {
+  depends_on = [module.vpc]
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.19.1"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.27"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+}
